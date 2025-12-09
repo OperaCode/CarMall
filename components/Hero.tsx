5ac94a6cd069c9e1f13586f1eeea1fc716cc8c47
Raphael Faboyinde <raphaelfaboyinde27@gmail.com>
2025-12-09 22:13:56 +0100

    folder/dependencies-structure

---
+++ components/Hero.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import image from "next/image";
+import CustomButton from "./CustomButton";
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">
+          {" "}
+          Find, book, or rent a car - quickly and easily!{" "}
+        </h1>
+        <p className="hero__subtitle">
+            Streamline your car rental experinece with our effortless booking process.
+        </p>
+
+
+
+    <CustomButton />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
