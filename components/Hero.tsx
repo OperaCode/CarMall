7d265f7f605b626569228dbb5487508509b9a2e6
Raphael Faboyinde <raphaelfaboyinde27@gmail.com>
2025-12-10 23:10:24 +0100

    navBar

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -22,15 +22,18 @@ const Hero = () => {
         <CustomButton
           title="Explore Cars"
           containerStyles="bg-primary-blue text-white rounded-full mt-10"
+          btnType="button"
           handleClick={handleScroll}
         />
       </div>
 
       <div className="hero__image-container ">
         <div className="hero__image">
-        <Image  src="/hero.png" alt="hero" fill className="object-contain"/>
+          <Image src="/hero.png" alt="hero" fill className="object-contain" />
 
-        <div className="hero__image-overlay"></div>
+          <div className="hero__image-overlay">
+
+          </div>
         </div>
       </div>
     </div>
